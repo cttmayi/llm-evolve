d9747f585084d164be3f774bb5ecf3cafc200371
ling.yuan <cttmayi@hotmail.com>
2025-08-31 13:58:03 +0800

    fix logging gbk error in windows

--- main.py
+++ main.py
@@ -4,6 +4,15 @@ Entry point script for OpenEvolve
 """
 import sys
 from openevolve.cli import main
+import logging
+
+
+logging.basicConfig(
+    # level=logging.INFO,
+    encoding='utf-8'
+)
+
+
 
 if __name__ == "__main__":
     sys.exit(main())
